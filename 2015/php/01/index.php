9303972229a22e565fef6befbcf87ff0398c2327
Jabrayilzade Ali <jabrayilzadeali@protonmail.com>
2026-07-23 19:22:53 +0400

    starting doing 2015 advent of code finished <day 1: Not Quite Lisp>

---
+++ 2015/php/01/index.php
@@ -0,0 +1,32 @@
+<?php
+
+$data = file_get_contents("data/data.txt");
+
+// function firstStar($data) {
+//     $floor = 0;
+//     for ($i = 0; $i < strlen($data); $i++) {
+//         if ($data[$i] === '(') {
+//             $floor++;
+//         } elseif ($data[$i] === ')') {
+//             $floor--;
+//         }
+//     }
+//     return $floor;
+
+// }
+
+function main($data, $secondStart = false) {
+    $floor = 0;
+    for ($i = 0; $i < strlen($data); $i++) {
+        if ($data[$i] === '(') {
+            $floor++;
+        } elseif ($data[$i] === ')') {
+            $floor--;
+        }
+        if ($floor === -1 && $secondStart) return $i + 1;
+    }
+    return $floor;
+
+}
+
+echo main($data);
