c2d78e0c44949479f6d04dda2c6294b159122f46
Jabrayilzade Ali <jabrayilzadeali@protonmail.com>
2026-07-29 22:42:28 +0400

    need to learn dynamic programming and apply here

--- 2015/07/php/main.php
+++ 2015/07/php/main.php
@@ -4,55 +4,105 @@ declare(strict_types=1);
 
 $data = file("../data/data.txt");
 
+function clearData($data) {
+    $variables = [];
+    $operations = [];
+    foreach ($data as $line) {
+        if (count(explode(' ', $line)) === 3) {
+            $variables[] = $line;
+            continue;
+        }
+        $operations[] = $line;
+    }
+    return [$variables, $operations];
+}
+
 $operations = [
     "123 -> x",
-    "x -> y",
-    // "x AND y -> d",
-    // "x OR y -> e",
-    // "x LSHIFT 2 -> f",
-    // "y RSHIFT 2 -> g",
-    // "NOT x -> h",
-    // "NOT y -> i",
+    "456 -> y",
+    "x AND y -> bn\r",   // note: two spaces before AND
 ];
 
 $result = [];
+
+function getValue(string $el) {
+    global $result;
+    return is_numeric($el) ? (int)$el : $result[$el] ?? null;
+}
 
 function lineParser(string $str) {
     global $result;
     $arr = explode(' ', $str);
     
-    print_r($arr);
-    
     if (count($arr) === 3) {
-        $result[$arr[array_key_last($arr)]] = is_numeric($arr[0]) ? $arr[0] : $result[$arr[0]];
+        $el = getValue($arr[0]);
+        if ($el === null) {
+            return false;
+        }
+        $result[$arr[array_key_last($arr)]] = $el;
     } elseif (count($arr) === 4) {
-        $result[$arr[array_key_last($arr)]] = ~ $arr[1];
+        $el = getValue($arr[1]);
+        if ($el === null) {
+            return false;
+        }
+        $result[$arr[array_key_last($arr)]] = ~ $el & 0xFFFF;
     } else {
-        $el1 = $arr[0];
-        $el2 = $arr[2];
+        $el1 = getValue($arr[0]);
+        $el2 = getValue($arr[2]);
+        if ($el1 === null || $el2 === null) {
+            // echo "+++++++++++++++++++++++++++++++ el1 $el1 | el2: $el2";
+            return false;
+        }
         $operator = $arr[1];
+        // echo '------------------------++++++++++: ', $el1, " $operator ", $el2, PHP_EOL;
         if ($operator === 'AND') {
-            return $el1 & $el2;
+            // echo 'anddfklasjfkljdasklfjdaslk;jflk', $el1,' ', $el2, ' ', $el1 & $el2, PHP_EOL;
+            $result[$arr[array_key_last($arr)]] = $el1 & $el2;
         } elseif ($operator === 'OR') {
-            return $el1 | $el2;
+            $result[$arr[array_key_last($arr)]] = $el1 | $el2;
         } elseif ($operator === 'LSHIFT') {
-            return $el1 << $el2;
+            $result[$arr[array_key_last($arr)]] = $el1 << $el2 ;
         } elseif ($operator === 'RSHIFT') {
-            return $el1 >> $el2;
+            $result[$arr[array_key_last($arr)]] = $el1 >> $el2;
         }
     }
-    return $result;
+    var_dump($result);
+    return true;
 }
-print_r(lineParser("123 -> x"));
-print_r(lineParser("456 -> y", $result));
 
 // echo lineParser("x AND y -> d");
 
-// foreach ($data as $line) {
-    
-// }
+// [$vars, $opers] = clearData($data);
+// unset($data[4]);
+// print_r($data);
 
-// $a = 123;
-// $b = 456;
+function main($data) {
+    // print_r($data);
+    $i = 0;
+    while (true) {
+        $line = $data[$i] ?? null;
+        if ($line) {
+            $val = lineParser($line);
+            if ($val) {
+                unset($data[$i]);
+                $data = array_values($data);
+                echo count($data), PHP_EOL;
+            }
+        }
+        $i++;
+        // echo $i, PHP_EOL;
+        if ($i >= count($data)) $i = 0;
+        // echo 'count: ', count($data), ' $i: ', $i, ' line: ', $line, PHP_EOL;
+        // echo 'Data: ';
+        if (count($data) === 0) break;
+    }
+}
 
-// echo $a << 2;
+main($data);
+var_dump($result);
+
+// $x = '123';
+// $y = '456';
+
+// // echo  ~ $x & 0xFFFF, PHP_EOL;
+// echo $x & $y, PHP_EOL;
